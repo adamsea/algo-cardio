4b9dd51039503e74fdc5640ba5192de740288582
Eric Adams <adams.ea@gmail.com>
2020-07-23 17:44:35 -0400

    tighten it up a bit

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -10,11 +10,7 @@ function maxChar(str) {
         .split('')
         .reduce((acc, c) => {
             let counter = acc.find(elem => elem.char === c);
-            if (counter) {
-                counter.count++;
-                return acc;
-            }
-            acc.push({ char: c, count: 1});
+            counter ? counter.count++ : acc.push({ char: c, count: 1});
             return acc;
         }, [])
         .sort((a, b) => a.count < b.count ? 1 : -1)[0].char;

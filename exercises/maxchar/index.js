8ce8770cb846347380e8a50998a09f0397764d43
Eric Adams <adams.ea@gmail.com>
2020-07-23 17:40:37 -0400

    maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,19 @@
 // maxChar("abcdddddddddd") === "d"
 // maxChar("bacon 1234444") === "4"
 
-function maxChar(str) {};
+function maxChar(str) {
+    return str
+        .split('')
+        .reduce((acc, c) => {
+            let counter = acc.find(elem => elem.char === c);
+            if (counter) {
+                counter.count++;
+                return acc;
+            }
+            acc.push({ char: c, count: 1});
+            return acc;
+        }, [])
+        .sort((a, b) => a.count < b.count ? 1 : -1)[0].char;
+};
 
 module.exports = maxChar;
